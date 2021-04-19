47314065c5ff7f15754355ca0f7b4302fb5be7c4
udobnaja <udobnaja@yandex-team.ru>
2021-04-19 15:01:45 +0300

    Initial Commit

---
+++ testuikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testuikit
+//
+//  Created by Anna Udobnaja on 19.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
